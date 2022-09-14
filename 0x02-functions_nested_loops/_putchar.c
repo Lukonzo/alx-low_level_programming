47e4d4535ccd74233e80153354d0e86dacd58607
Lukonzo <lukonzo.david@gmail.com>
2022-09-14 04:39:50 -0700

    Header file.

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,12 @@
-#include "unistd.h"
+#include <unistd.h>
+
 /**
- * _putchar - writes c to stdout
+ * _putchar - writes the character c to stdout 
  *
  * @c: The character to print
  *
  * Return: On success 1
- * on error, -1 is returnedand errno is set 
+ * on error, -1 is returned and errno is set appropriately
  */
 int _putchar(char c)
 {
