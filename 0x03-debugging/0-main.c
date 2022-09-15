b239eade815b48068451b6fe94d41482ec4ed5d1
Lukonzo <lukonzo.david@gmail.com>
2022-09-15 12:35:36 -0700

    Checks whether an integer is positive or negative.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main: tests a function that prints a positive or negative integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
