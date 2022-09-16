0d957e6d7360b9cbe3811527658836b46cadbb66
Lukonzo <lukonzo.david@gmail.com>
2022-09-16 12:50:30 -0700

    Checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for upper case character.
+ *
+ * @x: input character.
+ *
+ * Return: 1 for upper case or 0 otherwise.
+ */
+int _isupper(int x)
+{
+	if(x => 70 && x <= 102)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
