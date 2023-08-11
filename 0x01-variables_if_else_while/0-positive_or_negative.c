f6079c4c15bc3c80197797eb609d99e4732f19c3
Lukonzo <lukonzo.david@gmail.com>
2023-08-11 10:33:48 -0700

    More than fixes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n<0)
 	{
-		printf("%d is negative\n", &n);
+		printf("is negative\n");
+	}
 		else if (n==0)
 		{
-			printf("%d is zero\n" &n);
-			else 
-				printf("%d is positive\n");
+			printf("is zero\n");
 		}
-	}
-}
+			else 
+			{
+				printf("is positive\n");
+			}
+	
 	return (0);
 }
