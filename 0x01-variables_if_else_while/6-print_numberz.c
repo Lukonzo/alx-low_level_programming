fe25d86211984124b81472994bb63b2c54359031
Lukonzo <lukonzo.david@gmail.com>
2023-08-12 06:13:17 -0700

    Added a discription to main

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main: Entry point
+ * main - Entry point
  *
  * Task: Write a program that prints all single digit numbers
  * of base 10 starting from 0, followed by a new line.
