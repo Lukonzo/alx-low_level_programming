d661b709d9a73e9487f1cec7a481fd50326c704c
Lukonzo <lukonzo.david@gmail.com>
2023-08-12 06:23:30 -0700

    lowercase alphabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Task:  program that prints the lowercase
+ * alphabet in reverse, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return(0);
+}
