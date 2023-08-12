72c7b14c99e5155a3e2ef8aef830fe507ab897b1
Lukonzo <lukonzo.david@gmail.com>
2023-08-12 04:26:26 -0700

    Removed trailing whitespace, changed the description

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  *main: Entry point
  *
- * Task: A program that that prints the alphabet in lowercase, 
+ * Task: program that prints the alphabet in lowercase,
  * and then in uppercase, followed by a new line.
  *
  * Return: Always 0 (Success)
