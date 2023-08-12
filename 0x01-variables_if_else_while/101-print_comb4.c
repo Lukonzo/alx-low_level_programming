415f1b465e8124891dcd92dbe852c58fdb54c8d9
Lukonzo <lukonzo.david@gmail.com>
2023-08-12 11:22:35 -0700

    Removed a white space in line #6, some debugging including uae of tabs.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Task: A program that prints all possible 
+ * Task: A program that prints all possible
  * different combinations of three digits.
  *
  * Return: Always 0 (Success)
@@ -11,26 +11,26 @@
 
 int main(void)
 {
-        int m, n, o;
+	int m, n, o;
 
-        for (m = '0'; m <= '9'; m++)
-        {
-                for (n = '0'; n <= '9'; n++)
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (n = m + 1 ;n <= '9'; n++)
 		{
-			for (o = '0'; o <= '9'; o++)
+			for (o = n + 1; o <= '9'; o++)
 			{
-                                putchar(m);
-                                putchar(n);
+				putchar(m);
+				putchar(n);
 				putchar(o);
 
-                                if (m != '7' || n != '8' || o != '9')
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+				if (m != '7' || n != '8' || o != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
 }
