ef87c40dac5e7bd4100286db1f48d41b00d657b9
Lukonzo <lukonzo.david@gmail.com>
2023-08-16 16:00:04 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	for (m = 0; m <= 23; m++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
