7aa32c5bbe89ee73caba1cf04912524def89460f
Lukonzo <lukonzo.david@gmail.com>
2023-08-16 09:19:06 -0700

    Prints a-z dropped

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Program: prints alphabets in lower case
+ */
+
+int main(void)
+{
+	int c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
