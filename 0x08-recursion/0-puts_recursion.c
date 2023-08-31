ccd7d5b42d24b1d1c6de77772e7783dc62f328c4
Lukonzo <lukonzo.david@gmail.com>
2023-08-31 10:41:36 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+		_putchar('\n);
+	else
 	{
-		_putchar('\n');
-		return;
+		_putchar([0]);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
