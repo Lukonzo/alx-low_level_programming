76ba8d8fc2adf7351f8abbd99350c38b96fd921b
Lukonzo <lukonzo.david@gmail.com>
2023-09-24 01:11:38 -0700

    Last two of the mandatory tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - A function that adds two integers
+ * and returns the result
+ *
+ * @m: integer
+ * @n: integer
+ *
+ * Return: the sum of m, n.
+ */
+
+int add(int m, int n)
+{
+	int x;
+
+	x = m + n;
+
+	return (x);
+}
